744059e148c9cc0f03b760894d6720b62992998f
Jimmy <supermanlxvi@gmail.com>
2025-05-25 19:08:09 -0400

    Add Dockerfile and finalize webhook bot

--- MyNala_Telegram_Reward_Bot.py
+++ MyNala_Telegram_Reward_Bot.py
@@ -234,4 +234,4 @@ def set_webhook():
 if __name__ == "__main__":
     set_webhook()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
